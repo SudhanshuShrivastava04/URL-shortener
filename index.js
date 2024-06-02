7730130eb397e529743bbb86f06e0c5cb1c7f690
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-06-02 10:29:33 +0530

    URL shortener-02

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import urlRouter from "./routes/url.js";
+import staticRouter from "./routes/static.js";
 import { connectMongoDB } from "./connect.js";
+import path from "path";
+import { URL } from "./models/url.js";
 
 const app = express();
 const PORT = 8001;
@@ -11,6 +14,10 @@ connectMongoDB("mongodb://localhost:27017/short-url").then(() =>
 
 app.use(express.json());
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use("/url", urlRouter);
+app.use("/", staticRouter);
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
